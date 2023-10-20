4e9282629bdb804a7f573362a1a0f6e3427b0853
kyle cox <90216196+xocelyk@users.noreply.github.com>
2023-10-20 11:51:17 -0500

    Add files via upload

---
+++ streamlit-app.py
@@ -0,0 +1,22 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import streamlit as st
+
+test_text = st.text_input("Enter your text here", "Patient is sleepy")
+
+
+checkpoint = './results/save1'
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+inputs = tokenizer(test_text, return_tensors='pt', truncation=True, padding=True, max_length=512)
+
+
+# 2. Pass the processed input through the model
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+# 3. Get the predicted label
+# turn logits into probabilities
+probs = torch.nn.functional.softmax(logits, dim=-1)
+st.write(f"Probability Poor Historian: {probs[0][1].item()}")
